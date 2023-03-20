2da9f1d061ef00768660aa99b269baa4f7ce2e79
lindllgrn <lindllgrn@gmail.com>
2023-03-20 09:33:19 -0500

    Beginning the Basketball Scoreboard App.

---
+++ Section3_BB_Scoreboard/src/BB_Scoreboard_App.java
@@ -0,0 +1,13 @@
+
+public class BB_Scoreboard_App {
+
+	public BB_Scoreboard_App() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Player player1 = new Player("Debbie", 10);
+		System.out.println(player1.getJersey());
+	}
+
+}
