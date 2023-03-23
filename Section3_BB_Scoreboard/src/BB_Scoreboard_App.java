59bc883ddf8a287739c18f4d8d46734d5d1a3249
lindllgrn <lindllgrn@gmail.com>
2023-03-23 10:45:09 -0500

    completed team class

--- Section3_BB_Scoreboard/src/BB_Scoreboard_App.java
+++ Section3_BB_Scoreboard/src/BB_Scoreboard_App.java
@@ -1,13 +1,15 @@
-
 public class BB_Scoreboard_App {
 
+	
 	public BB_Scoreboard_App() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
 	public static void main(String[] args) {
-		Player player1 = new Player("Debbie", 10);
-		System.out.println(player1.getJersey());
+		
+		Team team1 = new Team();		
+		
+		System.out.println(team1.getTeamFouls());
 	}
 
 }
--- Section3_BB_Scoreboard/src/Team.java
+++ Section3_BB_Scoreboard/src/Team.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Team {
-	
+
 	private String name;
 	private List<Player> players;
 
@@ -10,24 +10,26 @@ public class Team {
 
 		this.name = "unknown";
 		this.players = new ArrayList<Player>();
-		
+
 	}
-	
+
 	public Team(String name) {
-		
+		this();
+		this.setName(name);
 	}
-	
+
 	public String getName() {
+
 		return this.name;
 	}
-	
+
 	public void setName(String name) {
-		//set the teams name 
+
 		this.name = name;
 	}
-	
+
 	public void addPlayer(String name, int jersey) throws Exception {
-		
+
 		Player duplicate = this.getPlayer(jersey);
 		if (duplicate == null) {
 			this.players.add(new Player(name, jersey));
@@ -36,9 +38,9 @@ public class Team {
 			throw new Exception("Jersey #" + jersey + " already assigned to " + duplicateName + "!");
 		}
 	}
-	
+
 	public Player getPlayer(int jersey) {
-		
+
 		int index = this.players.indexOf(jersey);
 		if (index == -1) {
 			return null;
@@ -48,35 +50,33 @@ public class Team {
 	}
 
 	public int getTeamPoints() {
-		//get the total number of points for the
-		//entire team by calling the Player.getPoints
-		//method
-		//return team points
 
+		Player teamPoints = new Player();
+
+		return teamPoints.getPoints();
 	}
-	
+
 	public int getTeamFouls() {
-		//get the total number of fouls for the 
-		//entire team using the Player.getFouls
-		//method
-		//return the total number of fouls for the team
+
+		Player teamFouls = new Player();
+
+		return teamFouls.getFouls();
 	}
-	
+
 	public void displayTeamStats() {
-		//display the team's summary
-		//stats using the Team.getTeamFouls
-		//and getTeamPoints methods
+
+		System.out.println(this.getTeamFouls() + " " + this.getTeamPoints());
 	}
-	
+
 	public void displayDetailStats() {
-		
+
 		for (int i = 0; i < players.size(); i++) {
-			
+
 			int jersey = players.get(i).getJersey();
 			String name = players.get(i).getName();
-					
+
 			System.out.println("#" + jersey + " " + name);
 		}
 	}
-	
+
 }
